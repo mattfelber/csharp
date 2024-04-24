7ae78b6194e05ffa48b39ea15dc910d723dc71b5
mfluxl <mfrancolux@gmail.com>
2024-04-23 23:43:04 -0300

    Random num guess game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,23 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Hello World!");
+
+        // Number guessing
+
+        Random randomGen = new Random();
+        int randomNumber = randomGen.Next(0,10);
+
+        int response;
+        do
+        {
+            Console.WriteLine("Guess the number");
+            response = int.Parse(Console.ReadLine());
+
+        } while (response != randomNumber);
     }
 }
