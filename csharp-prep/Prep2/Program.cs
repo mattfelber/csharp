591fd425c0589bb6eff9cd4aabaf641315013049
mfluxl <mfrancolux@gmail.com>
2024-04-23 22:39:05 -0300

    Grade calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
-class Program
+
+string lettergrade = "";
+Console.WriteLine("What's your grade");
+int grade = int.Parse(Console.ReadLine());
+string sign = "";
+
+if (grade >= 90)
 {
-    static void Main(string[] args)
+    lettergrade = "A";
+}
+
+else if (grade >= 80)
+{
+    lettergrade = "B";
+}
+else if (grade >= 70)
+{
+    lettergrade = "C";
+}
+else if (grade >= 60)
+{
+    lettergrade = "D";
+}
+else if (grade < 60)
+{
+    lettergrade = "F";
+}
+
+// Get the + or - sign for the grade
+if (grade > 60 || grade < 90)
+{
+    if (grade % 10 >= 7)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        sign = "+";
+    }
+    
+    else if (grade % 10 < 3)
+    {
+        sign = "-";
     }
 }
+
+Console.WriteLine($"Your grade is {lettergrade}{sign}.");
