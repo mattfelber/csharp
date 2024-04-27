21f5ab42f16e9b70d82081a2e19cc6291b6910ee
Matheus Felber <math.felber@gmail.com>
2024-04-27 01:07:09 -0300

    abstract job class

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
-    }
+        Job job = new Job("Software Developer", "EvilCorp", 2016, 2019);
+        
+        string company = job._companyName;
+        Console.WriteLine(company);
+}
 }
---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+using Microsoft.VisualBasic;
+
+public class Job
+{
+    public string _jobTitle;
+    public string _companyName;
+    public int _startYear;
+    public int _endYear;
+
+
+    public Job(string title, string company, int startyear, int endyear) 
+    {
+        _jobTitle = title;
+        _companyName = company;
+        _startYear = startyear;
+        _endYear = endyear;
+       
+
+}
+}
+/* public class Resume(string name, List<job>)
+{
+    return $"name: {name}\nJobs:{List<Job>}";
+} */
