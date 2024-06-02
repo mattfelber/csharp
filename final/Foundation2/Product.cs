d12570d0d7139c285cb7c21b1bff91ff2a91d78b
Matheus Felber <math.felber@gmail.com>
2024-06-02 16:54:06 -0300

    Product

---
+++ final/Foundation2/Product.cs
@@ -0,0 +1,25 @@
+public class Product
+{
+    private string name;
+    private int productId;
+    private double price;
+    private int quantity;
+
+    public Product(string name, int productId, double price, int quantity)
+    {
+        this.name = name;
+        this.productId = productId;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public string Name { get { return name; } }
+    public int ProductId { get { return productId; } }
+    public double Price { get { return price; } }
+    public int Quantity { get { return quantity; } }
+
+    public double GetTotalCost()
+    {
+        return price * quantity;
+    }
+}
