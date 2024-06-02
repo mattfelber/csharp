038c4c844bd4fd28dce288f2d6306ecefd9f533b
Matheus Felber <math.felber@gmail.com>
2024-06-02 17:13:13 -0300

    Address

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,25 @@
+public class Address
+{
+    private string streetAddress;
+    private string city;
+    private string stateProvince;
+    private string country;
+
+    public Address(string streetAddress, string city, string stateProvince, string country)
+    {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.stateProvince = stateProvince;
+        this.country = country;
+    }
+
+    public string StreetAddress { get { return streetAddress; } }
+    public string City { get { return city; } }
+    public string StateProvince { get { return stateProvince; } }
+    public string Country { get { return country; } }
+
+    public string GetFullAddress()
+    {
+        return $"{streetAddress}\n{city}, {stateProvince} {country}";
+    }
+}
